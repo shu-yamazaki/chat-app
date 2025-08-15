8c4be964451eb084fa57a18d02b0dc21b76329a6
山崎柊 <>
2025-08-15 13:27:43 +0900

    prisma依存させない修正をusecaseにも反映

--- backend/src/message/usecases/get-message.usecase.ts
+++ backend/src/message/usecases/get-message.usecase.ts
@@ -3,14 +3,14 @@
 import { Injectable } from '@nestjs/common';
 import {
   MessageRepository,
-  MessageWithUser,
+  Message,
 } from '@/message/repositories/message.repository';
 
 @Injectable()
 export class GetMessagesUseCase {
   constructor(private readonly messageRepository: MessageRepository) {}
 
-  async execute(roomId: string): Promise<MessageWithUser[]> {
+  async execute(roomId: string): Promise<Message[]> {
     return this.messageRepository.findMessagesByRoomId(roomId);
   }
 }
